c16f871cc52b8f7328ee0a83a32599d6e8a434aa
achiradesilva123 <achiradceyentra.com>
2024-09-19 20:31:18 +0930

    built a structure for AggregationServer to handle client requests simultaneously

--- src/main/java/com.assignment2/server/AggregationServer.java
+++ src/main/java/com.assignment2/server/AggregationServer.java
@@ -11,8 +11,36 @@ public class AggregationServer {
         try (ServerSocket serverSocket = new ServerSocket(DEFAULT_PORT)) {
             System.out.println("Aggregation Server started on port " + DEFAULT_PORT);
 
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                new Thread(new ClientHandler(clientSocket)).start(); // allocate a thread to handle client request simultaneously
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private static class ClientHandler implements Runnable{
+
+        private Socket clientSocket;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        @Override
+        public void run() {
+          try {
+              clientServiceHandler(clientSocket);
+          }catch (Exception e){
+              e.printStackTrace();
+          }
+        }
+    }
+
+    public static void clientServiceHandler(Socket clientSocket){
+        // handle get and put request here
+    }
+
 }
